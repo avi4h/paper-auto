c88eac2097898af036f83d16ab835976e060fa7f
Avi <93165148+avi4h@users.noreply.github.com>
2025-03-08 12:04:56 +0530

    just changed to telegram

--- config.py
+++ config.py
@@ -3,10 +3,8 @@ import os
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-SENDERS_NAME = "Cosmoo"
 DEVICE_CODE = "gs" # For windows use "gswin64c" for 64-bit or "gswin32c" for 32-bit
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendDocument"
-MAILGUN_API_URL = f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages"
 
 SAAMANA_BASE_URL = "https://epaper.saamana.com/download.php?file=https://enewspapr.com/News/SAMANA/PUN/{year}/{month}/{day}/{date_str}_{page}.PDF&pageno={page}"
 SAAMANA_MAX_PAGES = 30
